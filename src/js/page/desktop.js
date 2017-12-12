a3b1ddd0796c116f592b4f0be2aee2875ad86dda
18706380825 <18706380825@163.com>
2017-12-13 00:07:06 +0800

    新增框架，修改我的桌面部分内容

---
+++ src/js/page/desktop.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lixudong on 2017/12/12.
+ */
+var w = document.body.clientWidth;
+var h = document.body.clientHeight;
+$("#mainWrap").css({
+    "width":w,
+    "height":h
+});
+$(window).resize(function (a, b) {
+    var w = document.body.clientWidth;
+    var h = document.body.clientHeight;
+    $("#mainWrap").css({
+        "width":w,
+        "height":h
+    });
+});
